0bd85e2ee53bfe90f463e340d8aba473a7b0bd1b
Joel <simkalok0914@gmail.com>
2022-01-11 17:53:51 +0800

    Update MyRoom.ts

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -19,6 +19,8 @@ class MyRoomState extends Schema {
 
 //#region Room
 export class MyRoom extends Room {
+	// this.maxClients = 4;
+	
     // When room is initialized
     onCreate (options: any) {
         console.log("[MyRoom] onCreate:", {
@@ -28,7 +30,6 @@ export class MyRoom extends Room {
 
         this.setState(new MyRoomState());
         this.setSimulationInterval((deltaTime) => this.update(deltaTime));
-        // this.maxClients = 4;
     }
 
     // Authorize client based on provided options before WebSocket handshake is complete
